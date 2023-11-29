1157f723282f42a25f87fec4749662813a02ee37
Alexander Panferov <alexandr.n.panferov@gmail.com>
2023-11-29 20:14:58 +0700

    FBV ---> CBV

--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from catalog.models import Product
 
 
-def index(request):
-    product_list = Product.objects.all()
-    context = {
-        'object_list': product_list,
-        'title': 'Главная'
-    }
-    return render(request, 'catalog/index.html', context)
+class ProductListView(ListView):
+    model = Product
 
+
+class ProductDetailView(DetailView):
+    model = Product
 
 def contacts(request):
     if request.method == 'POST':
@@ -19,11 +18,3 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'{name} ({phone}): {message}')
     return render(request, 'catalog/contacts.html')
-
-
-def product(request, pk):
-    product_ = Product.objects.get(pk=pk)
-    context = {
-        'product': product_
-    }
-    return render(request, 'catalog/product.html', context)
