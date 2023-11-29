7a1eda4309c28db99c482b1560169f33271bc5b9
Alexander Panferov <alexandr.n.panferov@gmail.com>
2023-11-29 20:44:09 +0700

    add new model blog

--- catalog/models.py
+++ catalog/models.py
@@ -30,3 +30,20 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=150, verbose_name='заголовок')
+    slug = models.CharField(max_length=150, verbose_name='slug', **NULLABLE)
+    body = models.TextField(verbose_name='содержимое'),
+    preview = models.ImageField(upload_to='blog/', verbose_name='изображение', **NULLABLE)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=True, verbose_name='опубликовано')
+    views_count = models.IntegerField(default=0, verbose_name='просмотры')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'блог'
+        verbose_name_plural = 'блоги'
